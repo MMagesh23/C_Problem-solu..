f5569af39fd1ebc7bbfd9d4bd61b16d4462dded2
MMagesh23 <91615388+MMagesh23@users.noreply.github.com>
2022-03-01 20:29:44 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,29 @@
+/*WAP to Input 5 subject marks of a student and find total marks and
+percentage obtained by the student.*/
+
+int main()
+{
+    float T,E,M,S,SS,Total,P;
+
+    printf("\nEnter a tamil mark          : ");
+    scanf("%f",&T);
+
+    printf("\nEnter a English mark        : ");
+    scanf("%f",&E);
+
+    printf("\nEnter a Maths mark          : ");
+    scanf("%f",&M);
+
+    printf("\nEnter a Science mark        : ");
+    scanf("%f",&S);
+
+    printf("\nEnter a Social Science mark : ");
+    scanf("%f",&SS);
+
+    Total = T+E+M+S+SS;
+    P=(Total*100)/500;
+
+    printf("\nTotal      : %.2f ",Total);
+    printf("\nPercentage : %.2f",P);
+
+}
